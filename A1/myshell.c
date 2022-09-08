662ad06768d27ec6e5143cbef7e7c77c3e327ef4
EthanG78 <ethangarnier78@gmail.com>
2022-09-08 17:31:01 -0300

    Makefile now has variables for all source files and binary files the program may create

--- A1/myshell.c
+++ A1/myshell.c
@@ -1,6 +1,13 @@
+/*
+  myshell.c
+  COMP 3413 Operating Systems 1
+  
+  Ethan Garnier
+*/
 #include <stdio.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-    printf("Hello World");
+    printf("Hello World\n");
+    return 0;
 }

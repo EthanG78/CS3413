b31f918210f7eb94872d6df6c18694417102bd2e
EthanG78 <ethangarnier78@gmail.com>
2022-09-14 22:21:25 -0300

    implemented basic loop for a1 myshell

--- A1/myshell.c
+++ A1/myshell.c
@@ -5,6 +5,7 @@
   Ethan Garnier
 */
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <util.h>
@@ -13,28 +14,39 @@ int main(int argc, char *argv[])
 {
     char *inputStr;
     int nInput;
+
+    // this is the main shell loop which will accept
+    // user input, handle all errors, and end the 
+    // program when the user enters 'exit' 
     do
     {
+      // using the dynamic allocation conversion specifier
+      // so we do not need to specify buffuer size
       nInput = scanf("%ms", &inputStr);
 
       if (nInput == 1)
       {
         // successfully read user input
+        printf("You typed: %s\n", inputStr);
       }
       else if (errno != 0)
       {
-        // scanf thrown an error
+        // scanf threw an error,
+        // print out the errno error
+        perror("scanf()");
+        return 1;
       }
       else
       {
-        
+        // scanf did not read anything...
+        // todo: handle this
+        return 1;
       }
       
     } while (strcmp(inputStr, "exit") != 0);
 
-    // todo:
-    // since input is dynamically allocated,
-    // i think i need to free it
+    // since input is dynamically allocated
+    // we must call free on it
     free(inputStr);
     
     return 0;

cc4372d98aa105e9a33706fa52eb5178371b031a
EthanG78 <ethangarnier78@gmail.com>
2022-09-14 21:08:47 -0300

    WIP: started working on main loop in a1 myshell.c

--- A1/myshell.c
+++ A1/myshell.c
@@ -5,11 +5,37 @@
   Ethan Garnier
 */
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
 #include <util.h>
 
 int main(int argc, char *argv[])
 {
-    printf("Hello World\n");
-    printf("%d", TEST);
+    char *inputStr;
+    int nInput;
+    do
+    {
+      nInput = scanf("%ms", &inputStr);
+
+      if (nInput == 1)
+      {
+        // successfully read user input
+      }
+      else if (errno != 0)
+      {
+        // scanf thrown an error
+      }
+      else
+      {
+        
+      }
+      
+    } while (strcmp(inputStr, "exit") != 0);
+
+    // todo:
+    // since input is dynamically allocated,
+    // i think i need to free it
+    free(inputStr);
+    
     return 0;
 }

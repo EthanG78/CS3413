897d23258a371456bc614cf68477e7711f918251
EthanG78 <ethangarnier78@gmail.com>
2022-09-19 17:57:48 -0300

    finished myshell.c in lab2

--- LAB2/myshell.c
+++ LAB2/myshell.c
@@ -10,7 +10,11 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
 
 #define BUFSIZE 81
 
@@ -18,25 +22,40 @@ int main(int argc, char *argv[]){
 
   int ret_code;		// return code
   int len;		// length of entered command
+  int pid;		// process pid
   char buffer[BUFSIZE];	// room for 80 chars plus \0
   char *cmd;		// pointer to entered command
 
   // Print a prompt and read a command from standard input
   printf("Enter a command: > ");
   cmd = fgets(buffer, BUFSIZE, stdin);
-
+  
   // did the user enter a command?
-  if(cmd != NULL){
+  while(cmd != NULL){
     // check for the newline character and overwrite with \0
     len = strlen(buffer);
     if(buffer[len-1] == '\n'){
       buffer[len-1] = '\0';
     }
-    // execute the command
-    ret_code = execlp(cmd, cmd, NULL);
-    if(ret_code != 0){
-      printf("Error executing %s.\n", cmd);
+
+    pid = fork();
+    if (pid != 0)
+    {
+      // parent process
+      waitpid(pid, NULL, 0);
     }
+    else
+    {
+      // execute the command
+      ret_code = execlp(cmd, cmd, NULL);
+      if(ret_code != 0){
+        printf("Error executing %s.\n", cmd);
+        exit(0);
+      }
+    }
+    printf("Enter a command: > ");
+    cmd = fgets(buffer, BUFSIZE, stdin);
   }
   printf("All done.\n");
+  return 0;
 } // main

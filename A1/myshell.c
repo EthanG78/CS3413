76397c24f8411b45ee8c888663d43da245bbdff0
EthanG78 <ethangarnier78@gmail.com>
2022-09-26 19:40:55 -0300

    wip: trying a new implementation of piping!

--- A1/myshell.c
+++ A1/myshell.c
@@ -9,23 +9,27 @@
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
+#include <math.h>
 #include <util.h>
 
 int main(int argc, char *argv[])
 {
   char *cwd;              // pointer to current working dir string
   char *inputStr;         // pointer to entered cmd
-  char **pipeArr;         // arry of entered pipes
-  int nPipes;             // number of pipes in pipeArr;
+  char **commandArr;      // arry of entered commands
+  int nCommands;          // number of commands in pipeArr
+  int nPipes;             // number of pipes used by user input
   char **cmdArr;          // array of entered cmd args
   int nArgs;              // number of args in cmdArr
   char buffer[INPUT_MAX]; // max input buffer
   pid_t pid;              // process id
   int i;                  // pipe iter
-  int pfd[2];             // pipe file descriptors
+  int *pfds;              // pipe file descriptors
+  int pipeRIdx;           // current pipe read index
+  int pipeWIdx;           // current pipe write index
 
   // review the amount of data we malloc
-  pipeArr = (char **)malloc(CMD_MAX * INPUT_MAX);
+  commandArr = (char **)malloc(CMD_MAX * INPUT_MAX);
   cmdArr = (char **)malloc(CMD_MAX * INPUT_MAX);
 
   // call getcwd() to get the shell's
@@ -34,13 +38,6 @@ int main(int argc, char *argv[])
   if (cwd == NULL)
   {
     perror("getcwd");
-    return EXIT_FAILURE;
-  }
-
-  // open pipe file descriptors
-  if (pipe(pfd) == -1)
-  {
-    perror("pipe()");
     return EXIT_FAILURE;
   }
 
@@ -60,21 +57,40 @@ int main(int argc, char *argv[])
     }
 
     // tokenize user input based on the pipe '|' delimeter and
-    // iterate through each of the pipes the user has entered
-    nPipes = tokenizeIntoArr(inputStr, pipeArr, CMD_MAX, "|");
+    // iterate through each of the commands the user has entered
+    nCommands = tokenizeIntoArr(inputStr, commandArr, CMD_MAX, "|");
+
+    // TODO:
 
     // Here is where we will handle builtin functions
     // ..
 
-    // Here is where we will fork a new process that acts as a subshell
-    // to execute the pipes. This will be the process our main process
-    // has to wait on. We can close pfd after we create the subshell. 
+    // based on how many commands were entered by the user,
+    // we can determine how many pipes were used
+    nPipes = nCommands - 1;
 
+    // allocate space in the pipe file descriptors
+    // variable based on how many pipes there are
+    pfds = (int *)malloc(nPipes * sizeof(int) * 2);
 
+    // iterate through each of the entered pipes
+    // and create a pipe file descriptor
     for (i = 0; i < nPipes; i++)
     {
-      // pipeArr[i] becomes the command string we want to split
-      nArgs = tokenizeIntoArr(pipeArr[i], cmdArr, CMD_MAX, " ");
+      if (pipe(pfds[i * 2]) == -1)
+      {
+        perror("pipe()");
+        return EXIT_FAILURE;
+      }
+    }
+
+    // iterate through each command and execute it
+    // making sure to modify the stdin and stdout of each
+    // process based on what pipe we are on
+    for (i = 0; i < nCommands; i++)
+    {
+      // commandArr[i] becomes the command string we want to split
+      nArgs = tokenizeIntoArr(commandArr[i], cmdArr, CMD_MAX, " ");
 
       if (nArgs > 0)
       {
@@ -114,6 +130,9 @@ int main(int argc, char *argv[])
           // executing commands
           pid = fork();
 
+          pipeRIdx = (i - 1) * 2;
+          pipeWIdx = i * 2 + 1;
+
           if (pid < 0)
           {
             perror("fork()");
@@ -128,15 +147,19 @@ int main(int argc, char *argv[])
             // hangs after. probably waiting
             // on input pipe....
             // SOLUTION: we need to close pfd
-            // in the main process BEFORE we wait 
+            // in the main process BEFORE we wait
             // but will this mess up the forks??
 
             // redirect the stdout of the command that is about
             // to be executed to the write end of the pipe if
             // there is another command in the pipe array
-            if (i < nPipes - 1)
+            if (i < nCommands - 1)
             {
-              dup2(pfd[1], STDOUT_FILENO);
+              if (dup2(pfds[pipeWIdx], STDOUT_FILENO) == -1)
+              {
+                perror("dup2()");
+                return EXIT_FAILURE;
+              }
             }
 
             // redirect the stdin of the command that is about
@@ -145,14 +168,18 @@ int main(int argc, char *argv[])
             // before this one
             if (i > 0)
             {
-              dup2(pfd[0], STDIN_FILENO);
+              if (dup2(pfds[pipeRIdx], STDIN_FILENO) == -1)
+              {
+                perror("dup2()");
+                return EXIT_FAILURE;
+              }
             }
 
             // close both pipe file descriptors since
             // they have either been duped to stdin/out
             // or they are not being used
-            close(pfd[0]);
-            close(pfd[1]);
+            close(pfds[pipeRIdx]);
+            close(pfds[pipeWIdx]);
 
             // execute the command with argument
             // array that was created
@@ -162,6 +189,22 @@ int main(int argc, char *argv[])
               perror("execvp()");
               exit(EXIT_SUCCESS);
             }
+          }
+
+          // close any pipes associated with the
+          // process we just ran
+          if (i > 0)
+          {
+            // close the associated read end of the
+            // pipe that was used as input for this process
+            close(pfds[pipeRIdx]);
+          }
+
+          if (i < nCommands - 1)
+          {
+            // close the associated write end of the
+            // pipe that was used as output for this process
+            close(pfds[pipeWIdx]);
           }
 
           // wait for child process to finish
@@ -176,17 +219,17 @@ int main(int argc, char *argv[])
 
   printf("Exiting shell\n");
 
-  // close pipe file descriptors
-  close(pfd[0]);
-  close(pfd[1]);
-
   // free the allocated space for arrays
-  free(pipeArr);
+  free(commandArr);
   free(cmdArr);
 
   // since cwd is dynamically allocated
   // we must call free on it
   free(cwd);
+
+  // free the allocated space
+  // for pipe file descriptors
+  free(pfds);
 
   return EXIT_SUCCESS;
 }

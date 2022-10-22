f7754c55d3d976142b1521b57746295e6bafcc08
EthanG78 <ethangarnier78@gmail.com>
2022-10-22 10:47:14 -0300

    added definition to enable pselect

--- A2/player.c
+++ A2/player.c
@@ -1,10 +1,13 @@
+// for pselect?
+#define __USE_XOPEN2K
+
 #include <stdio.h>
 #include <errno.h>
 #include <math.h>
 #include <string.h>
+#include <sys/select.h>
 #include <sys/time.h>
 #include <sys/types.h>
-#include <sys/select.h>
 #include <unistd.h>
 
 #include "player.h"

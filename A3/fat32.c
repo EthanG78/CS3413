ea568039a489f1a0a1078a47c9457a4ab86c6673
EthanG78 <ethangarnier78@gmail.com>
2022-11-18 13:35:01 -0400

    have the boot sector and bpb being read from fat32 file. printing the bytes per sector as test

--- A3/fat32.c
+++ A3/fat32.c
@@ -1,18 +1,28 @@
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
 #include "fat32.h"
 
 fat32Head* createHead(int fd)
 {
-    fat32BS *bootSector;
-    int nBytesRead = read(fd, bootSector, sizeof(fat32BS));
+    // todo: does this need to be a pointer?
+
+    // create a new fat32BS variable and
+    // initialize each of each fields to 0s
+    fat32BS bootSector;
+    memset(&bootSector, 0, sizeof(fat32BS));
+
+    // read the boot sector and bpb from top of fat32 file
+    int nBytesRead = read(fd, &bootSector, sizeof(fat32BS));
     if (nBytesRead == -1)
     {
         perror("error reading fat32 boot sector");
         return NULL;
     }
     
-    printf("%d", bootSector->BPB_BytesPerSec);
+    printf("%d", bootSector.BPB_BytesPerSec);
 
     return NULL;
 }
